d55349295790482049b333d98042880d7621519a
Najeerahammad Masali <Najeerahammad.Masali@XEBIAINDIA.LOCAL>
2021-02-22 19:35:28 +0530

    comp is created

---
+++ src/app/components/account-summary-details/account-summary-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-summary-details',
+  templateUrl: './account-summary-details.component.html',
+  styleUrls: ['./account-summary-details.component.css']
+})
+export class AccountSummaryDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/budget-summary-details/budget-summary-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-summary-details',
+  templateUrl: './budget-summary-details.component.html',
+  styleUrls: ['./budget-summary-details.component.css']
+})
+export class BudgetSummaryDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
